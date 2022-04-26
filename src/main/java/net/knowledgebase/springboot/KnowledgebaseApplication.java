adbb5c8c52ac68199af2f4d18e9d4dba66f979d0
ShaneConcannon <shaneconcannon@gmail.com>
2022-04-26 17:50:17 +0100

    extends SpringBootServletInitializer

--- src/main/java/net/knowledgebase/springboot/KnowledgebaseApplication.java
+++ src/main/java/net/knowledgebase/springboot/KnowledgebaseApplication.java
@@ -2,9 +2,10 @@ package net.knowledgebase.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KnowledgebaseApplication {
+public class KnowledgebaseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KnowledgebaseApplication.class, args);
